ad84d616f546fd09d6a6b7264fdb8684a08d585c
Casper S Poulsen <cspoulsen@hotmail.com>
2019-07-30 09:17:21 +0200

    overwriting master

--- tempcode.R
+++ tempcode.R
@@ -1,6 +1,9 @@
 'data1 <- iris
+data1 <- iris
 head(data1)
 colnames(data1)
 colSums(data1[,1:4])
 colSums(data1[1:5,1:4])
+colSums(data1[1:6,1:4])
+data1$Petal.area <- data1[,3]*data1[,4]
 colSums(data1[1:6,1:4])
