6501b01540287a35933817cb389650bde379170f
Casper S Poulsen <cspoulsen@hotmail.com>
2019-07-30 10:27:53 +0200

    Update tempcode.R

--- tempcode.R
+++ tempcode.R
@@ -8,4 +8,4 @@ data1$Petal.area <- data1[,3]*data1[,4]
 colSums(data1[1:6,1:4])
 data1$Petal.area <- data1[,3]*data1[,4]
 print("very good")
-data1$new <- data1[,3]*data1[,4]*2
+data1$new <- data1[,3]*data1[,4]*3
